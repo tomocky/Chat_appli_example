90c3c55b3ed6d725716a2c38f8776f1b88fd7ffb
tom <tomokinooniku1@hotmail.com>
2017-02-24 17:17:17 +0900

    add of cs file

---
+++ ChatApplication/Assets/Script/StateController.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+
+namespace chat{
+	public class StateController : MonoBehaviour {
+	
+		//スレッドのクローン元
+		[SerializeField]private GameObject GuildClone;
+		[SerializeField]private GameObject RoomClone;
+		[SerializeField]private GameObject PrivateClone;
+	
+		//クローンを行う先のオブジェクト
+		[SerializeField]private GameObject GuildParent;
+		[SerializeField]private GameObject RoomParent;
+		[SerializeField]private GameObject PrivateParent;
+	
+		//
+	
+		// Use this for initialization
+		void Start () {
+			int i;
+			for (i = 0 ; i < 5 ; i++)
+			{
+				var obj_guild = Instantiate (GuildClone);
+				obj_guild.transform.parent = GuildParent.transform;
+				obj_guild.transform.localScale = Vector3.one;
+				obj_guild.SetActive (true);
+
+		
+			}
+		}
+
+		// Update is called once per frame
+		void Update () {
+		
+		}
+	}
+} 
